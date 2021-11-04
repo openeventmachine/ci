b2a513ccfd4edb8510435b9af897831d1623e3af
Lifang Zhang <lifang.zhang@nokia.com>
2021-11-04 14:47:44 +0200

    scripts: remove unused scripts
    env.sh was used by Travis ci
    docker.sh is not used by the updated ci workflows of eventmachine
    test.sh is moved to openeventmachine/em-odp/scripts/robot_test.sh

--- docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install tzdata -y
-apt-get install automake autoconf libconfig-dev libssl-dev libtool pkg-config python3-pip git sudo -y
-pip3 install robotframework
